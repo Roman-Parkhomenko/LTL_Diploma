e966a21a9b28701ef52db458561ff8185bf5bdbe
Booblyk <be.th.i.wa.be@gmail.com>
2023-04-13 00:13:07 +0300

    Best of working(90 sec response)

---
+++ app/build/generated/source/buildConfig/release/info/Parkhomenko/personaldiary/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.Parkhomenko.personaldiary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "info.camposha.personaldiary";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
