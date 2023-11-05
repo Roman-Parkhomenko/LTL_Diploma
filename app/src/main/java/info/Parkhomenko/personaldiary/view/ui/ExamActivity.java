bd5648855f6d6511c20647b4f9c3879e88aa35fb
Booblyk <be.th.i.wa.be@gmail.com>
2023-11-05 19:39:39 +0200

    ExamActivity

---
+++ app/src/main/java/info/Parkhomenko/personaldiary/view/ui/ExamActivity.java
@@ -0,0 +1,168 @@
+package info.Parkhomenko.personaldiary.view.ui;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import info.Parkhomenko.personaldiary.R;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class  ExamActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    JSONArray messageArray;
+    MessageAdapter messageAdapter;
+    int i =0;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(90, TimeUnit.SECONDS)
+            .build();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+        messageList = new ArrayList<>();
+        messageArray = new JSONArray();
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_btn);
+
+        //setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v) -> {
+            String question = messageEditText.getText().toString().trim();
+
+            addToChat(question, Message.SENT_BY_ME);
+
+            messageEditText.setText("");
+            callAPI(question);
+            welcomeTextView.setVisibility(View.GONE);
+        });
+    }
+
+    void addToChat(String message, String sentBy) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+
+            }
+        });
+    }
+
+    void addResponse(String response) {
+        messageList.remove(messageList.size() - 1);
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String question) {
+        //okhttp
+        messageList.add(new Message("Typing... ", Message.SENT_BY_BOT));
+
+        JSONObject jsonBody = new JSONObject();
+
+
+        JSONObject obj = new JSONObject();
+        try {
+            jsonBody.put("model", "gpt-3.5-turbo");
+
+
+            obj.put("role", "user");
+
+            if (i<1) {
+                obj.put("content", "You are an examiner. Please ask me some questions for the topic: " + question +
+                        "\\nFor example:\n" +
+                        "1. You asking\n" +
+                        "2. I answer\n" +
+                        "3. you're saying if my answer was correct. If I was incorrect say \"Incorrect!\" " +
+                        "and critique my response to each question and provide example answers then ask another question. " +
+                        "If I was correct say \"Correct!\" and ask another question.\n" +
+                        "Begin by asking the first question and then waiting for my response. ");
+                i++;
+            } else {
+                obj.put("content", question);
+            }
+            messageArray.put(obj);
+            jsonBody.put("messages", messageArray);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer d")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to " + e.getMessage() + "Sent the type of error to HelpLivetoLearn@gmail.com");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content");
+                        addResponse(result.trim());
+                        JSONObject obj = new JSONObject();
+
+                        obj.put("role", "assistant");
+                        obj.put("content", result);
+                        messageArray.put(obj);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else {
+                    addResponse("Failed to load response due to " + response.body().toString());
+                }
+            }
+        });
+    }
+}
